dfc41af770c69a43ead05dcb520bda89d766d613
Michał Kardasz <michalkardasz03@gmail.com>
2025-11-30 15:15:32 +0100

    fix ad_executor

--- infra/ad_executor.py
+++ infra/ad_executor.py
@@ -1,9 +1,24 @@
 import json
 import subprocess
+from pathlib import Path
 
 from excel_fetcher import ExcelFetcher
 
 class AdExecutor:
+
+    @staticmethod
+    def resolve_scripts_path():
+        # Looking for a file locally
+        # It has to be seated in 'excel' folder
+        project_root = Path(__file__).resolve().parents[1]
+
+        # sets a path where excel should be located
+        scripts_dir = project_root / "scripts"
+
+        if not scripts_dir:
+            raise FileNotFoundError(f"No scripts folder in {scripts_dir}")
+        return scripts_dir
+
 
     @staticmethod
     def is_list_of_tuples(value):
@@ -23,10 +38,10 @@ class AdExecutor:
             ps_array = "@(" + ",".join(
                 f'@("{t[0]}", "{t[1]}")' for t in tuples
             ) + ")"
-
+            print(ps_array)
             ps_cmd = f"""
             $tuples = {ps_array}
-            . .\\getReferenceUserGroups.ps1
+            . "{AdExecutor.resolve_scripts_path()}\\getReferenceUserGroups.ps1"
             Get-UserGroupsFromTuples -UserTuples $tuples
             """
 
@@ -49,5 +64,6 @@ class AdExecutor:
 
 
 fetcher = ExcelFetcher()
+print(AdExecutor.resolve_scripts_path())
 #print(fetcher.rows) this is not up to date
 AdExecutor.get_reference_user_groups(fetcher.rows)
